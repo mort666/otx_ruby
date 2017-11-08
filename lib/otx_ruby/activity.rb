54ac0bfc1d871c0b8d8b4515050fd0e311a6298d
saikez <saikez@yahoo.com>
2017-11-08 02:13:50 +0000

    Add Pulse Activity calls
    Added `get_all` and `get_since`

---
+++ lib/otx_ruby/activity.rb
@@ -0,0 +1,66 @@
+module OTX
+  #
+  # Within the OTX system you are able to subscribe to pulses from other users,
+  # this class allows the retreival of the currently subscribed pulse feeds and 
+  # the associated pulses, as well as pulses from followed Users
+  #
+  class Activity < OTX::Base
+    #
+    # Get all pulses activity from the API, get all events in chunks defined by
+    # limit
+    #
+    # @param limit [Integer] Size of chunk of data to be Returned (default = 20)
+    # @return [Array<OTX::Pulse>] Parsed Pulses
+    #
+    def get_all(limit=20)
+      uri = '/api/v1/pulses/activity'
+      params = {limit: limit}
+      pulses = []
+      begin
+        json_data = get(uri, params)
+        page = json_data['next']
+
+        params = URI::decode_www_form(URI(page).query).to_h unless page.nil?
+
+        pulses += json_data['results']
+      end while !page.nil?
+
+      results = []
+      pulses.each do |pulse|
+        results << OTX::Pulse.new(pulse)
+      end
+
+      return results
+    end
+
+    #
+    # Get all pulses activity from the API, get all events in chunks defined by
+    # limit and since timestamp
+    #
+    # @param timestamp [Time] Timestamp of point in time to get records since in 
+    # ISO Format
+    # @param limit [Integer] Size of chunk of data to be Returned (default = 20)
+    # @return [Array<OTX::Pulse>] Parsed Pulses
+    # 
+    def get_since(timestamp, limit=20)
+      uri = '/api/v1/pulses/activity'
+      params = {limit: limit, modified_since: timestamp}
+      pulses = []
+      begin
+        json_data = get(uri, params)
+        page = json_data['next']
+
+        params = URI::decode_www_form(URI(page).query).to_h unless page.nil?
+
+        pulses += json_data['results']
+      end while !page.nil?
+
+      results = []
+      pulses.each do |pulse|
+        results << OTX::Pulse.new(pulse)
+      end
+
+      return results
+    end
+  end
+end
