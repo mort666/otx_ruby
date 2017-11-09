e2a08db77bc03dd328098248e1b394c975b208b0
saikez <saikez@yahoo.com>
2017-11-09 01:45:17 +0000

    Add Correlation-rule related calls

---
+++ lib/otx_ruby/types/correlation_rule.rb
@@ -0,0 +1,17 @@
+module OTX
+  module Indicators
+    class CorrelationRule < OTX::Type::Base
+      def initialize(attributes={})
+        attributes.each do |key, value|
+          if key != 'pulse_info' || key != 'base_indicator'
+            send("#{key.downcase}=", value)
+          elsif key == 'pulse_info'
+            send("#{key.downcase}=", OTX::Indicator::CVE::PulseInfo.new(value))
+          else
+            send("#{key.downcase}=", OTX::Indicator::CVE::BaseIndicator.new(value))
+          end
+        end
+      end
+    end
+  end
+end
